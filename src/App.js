d33f89e5be90c005f1166a7313d2f420479f2ab1
PL-Kenshin <78429627+PL-Kenshin@users.noreply.github.com>
2022-04-29 18:26:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ComponentQuiz from "./components/ComponentQuiz"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+ return (
+   <ComponentQuiz/>
+ );
+}
+
+export default App;
